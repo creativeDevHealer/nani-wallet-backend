692cd656d9e71296232f09dfc705be1da5b99491
creativeDevHealer <code.healer.1120@gmail.com>
2026-01-18 22:53:52 -0800

    cors fixed

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization', 'x-request-id']
 }))
 
-app.options("*", cors());
+app.options(/.*/, cors());
 // Body parsing middleware
 app.use(express.json({ limit: "50mb" }))
 app.use(express.urlencoded({ extended: true, limit: "50mb" }))
